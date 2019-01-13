6459679d3e37177c9cc987140a99dd6bbbd765aa
Ivar Grytten <ivar.grytten@gmail.com>
2019-01-13 16:09:49 +0100

    Initial setup

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from setuptools import setup
 
-setup(name='rough_graph_mapper',
+setup(name='two_step_graph_mapper',
       version='0.0.1',
-      description='Rough Graph Mapper',
-      url='http://github.com/uio-bmi/rough_graph_mapper',
+      description='Two Step Graph Mapper',
+      url='http://github.com/uio-bmi/two_step_graph_mapper',
       author='Ivar Grytten and Knut Rand',
       author_email='',
       license='MIT',
       zip_safe=False,
-      install_requires=['numpy', 'python-coveralls',
+      install_requires=['numpy', 'python-coveralls', 'pyvg',
                         'pyfaidx', 'offsetbasedgraph', 'graph_peak_caller', 'tqdm'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
       entry_points = {
-        'console_scripts': ['rough_graph_mapper=rough_graph_mapper.command_line_interface:main'],
+        'console_scripts': ['two_step_graph_mapper=two_step_graph_mapper.command_line_interface:main'],
       }
 
       )
