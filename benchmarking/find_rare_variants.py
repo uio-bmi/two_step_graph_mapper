0d1112979ee27356df8d0120e9c7a7eb1c9ea981
Ivar Grytten <ivar.grytten@gmail.com>
2019-01-30 20:14:45 +0100

    Div fixes

--- benchmarking/find_rare_variants.py
+++ benchmarking/find_rare_variants.py
@@ -2,21 +2,37 @@ import offsetbasedgraph.vcfmap as vcfmap
 import offsetbasedgraph as obg
 import logging
 logging.basicConfig(level=logging.INFO)
+import re
 
 def get_alle_freq(parts):
-    AF = parts[7].split(";")[1].split("=")
-    assert AF[0] == "AF", (parts, AF)
+    hit = re.search('AF=([0-9.,]+)', parts[7])
+    if hit is None:
+        logging.error("Found no AF on line %s"  % parts)
+    AF = hit.group(1)
+
     try:
-        return max(float(af) for af in AF[1].split(","))
+        return max(float(af) for af in AF.split(","))
     except:
-        print(parts, AF)
+        logging.error("Could not parse allele frequency.")
+        logging.error(parts)
+        logging.error(AF)
         raise
 
 
 def get_common_variants(vcf_filename):
+    logging.info("Get common variants")
     lines = (line for line in open(vcf_filename) if not line.startswith("#"))
     parts = (line.split("\t") for line in lines)
-    ids = (p for part in parts if get_alle_freq(part)>0.01 for p in part[2].split(";"))
+    ids = set()
+    for j, part in enumerate(parts):
+        if j % 100000 == 0:
+            logging.info("%d lines parsed " % j)
+
+        if get_alle_freq(part) > 0.01:
+            for p in part[2].split(";"):
+                ids.add(p)
+
+    #ids = (p for part in parts if get_alle_freq(part)>0.01 for p in part[2].split(";"))
     return set(ids)
 
 
@@ -29,11 +45,21 @@ def tmp(chromosome, folder, vcf_name):
 
 
 def get_rare_node_ids(all_vcf, individual_vcf, entry_to_edge):
+    logging.info("Get rare node ids")
     common_variants = get_common_variants(all_vcf)
-    print(len(common_variants))
     vcf_entries = vcfmap.get_vcf_entries(individual_vcf, common_variants)
     variants = (entry_to_edge(entry) for entry in vcf_entries)
-    nodes = (node for var in variants if type(var) in (vcfmap.INS, vcfmap.SNP) for node in var.nodes)
+    nodes = set()
+
+    for j, var in enumerate(variants):
+        if j % 100000 == 0:
+            logging.info("%d variants parsed" % j)
+
+        if type(var) in (vcfmap.INS, vcfmap.SNP):
+            for node in var.nodes:
+                nodes.add(node)
+
+    #nodes = (node for var in variants if type(var) in (vcfmap.INS, vcfmap.SNP) for node in var.nodes)
     return set(nodes)
 
 
@@ -75,3 +101,4 @@ if __name__ == "__main__":
             print(name + "\t" + "0")
 
     logging.info("%d rare nodes" % len(rare_nodes))
+
